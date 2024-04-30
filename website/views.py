3b558208d2790aa47b0964b102be60b51d309a16
Heulen <heulen98@gmail.com>
2024-04-30 21:31:29 +0200

    Added printer duplicate prevention functionality

--- website/views.py
+++ website/views.py
@@ -17,55 +17,66 @@ serial = os.getenv('SERIAL', '01P09A3A2700815')
 
 bambu_client = BambuClient(hostname, access_code, serial)
 
+
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    if request.method == 'POST': 
-        note = request.form.get('note')#Gets the note from the HTML 
+    if request.method == 'POST':
+        note = request.form.get('note')  # Gets the note from the HTML
 
         if len(note) < 1:
-            flash('Note is too short!', category='error') 
+            flash('Note is too short!', category='error')
         else:
-            new_note = Note(data=note, user_id=current_user.id)  #providing the schema for the note 
-            db.session.add(new_note) #adding the note to the database 
+            # providing the schema for the note
+            new_note = Note(data=note, user_id=current_user.id)
+            db.session.add(new_note)  # adding the note to the database
             db.session.commit()
             flash('Note added!', category='success')
-    
+
     printer_files = bambu_client.get_files()
     for file in printer_files:
-            print(file)
+        print(file)
 
     return render_template("home.html", user=current_user)
+
 
 @views.route('/printer', methods=['GET', 'POST'])
 @login_required
 def printer():
-    if request.method == 'POST': 
-        name = request.form.get('Name')                 #Gets the info from the HTML
+    if request.method == 'POST':
+        name = request.form.get('Name')  # Gets the info from the HTML
         hostname = request.form.get('Hostname')
         access_code = request.form.get('AccessCode')
         serial = request.form.get('Serial')
 
+        printer = Printer.query.filter_by(serial=serial).first()
+        if printer:
+            flash('Printer already exists', category='error')
+        printer = Printer.query.filter_by(name=name).first()
+        if printer:
+            flash('Printer name already in use', category='error')
 
-        if len(name) < 1 or len(hostname) < 1 or len(access_code) < 1 or len(serial) < 1:
+        elif len(name) < 1 or len(hostname) < 1 or len(access_code) < 1 or len(serial) < 1:
             flash('One or more fields are too short!', category='error')
 
         else:
-            new_printer = Printer(name=name, hostname=hostname, access_code=access_code, serial=serial, user_id=current_user.id)
+            new_printer = Printer(name=name, hostname=hostname,
+                                  access_code=access_code, serial=serial, user_id=current_user.id)
             db.session.add(new_printer)
             db.session.commit()
             flash('Printer added!', category='success')
 
-    
     printer_files = bambu_client.get_files()
     for file in printer_files:
-            print(file)
-            
+        print(file)
+
     return render_template("printer.html", user=current_user)
 
+
 @views.route('/delete-note', methods=['POST'])
-def delete_note():  
-    note = json.loads(request.data) # this function expects a JSON from the INDEX.js file 
+def delete_note():
+    # this function expects a JSON from the INDEX.js file
+    note = json.loads(request.data)
     noteId = note['noteId']
     note = Note.query.get(noteId)
     if note:
@@ -73,6 +84,5 @@ def delete_note():
             db.session.delete(note)
             db.session.commit()
             flash('Note removed!', category='error')
-
 
     return jsonify({})
